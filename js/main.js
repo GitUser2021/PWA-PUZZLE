76cb3c1a3dcf070c7a82b4da91b36bd5d08a30c4
GitUser2021 <58796929+GitUser2021@users.noreply.github.com>
2020-01-30 04:44:03 -0300

    agrego SweetAlert2 para preguntar si desea cancelar el juego

--- js/main.js
+++ js/main.js
@@ -38,12 +38,8 @@ function show_image(show) {
 }
 
 // boton New Game.
-document.getElementById('btnNewGame').addEventListener('touchstart', () => {
-    reset()
-    start()
-    setTimeout(() => {
-        scramble(500)
-    }, 0);
+document.getElementById('btnNewGame').addEventListener('click', () => {
+    pregunta(puzzle_size,true)
 })
 
 // boton Help.
@@ -55,34 +51,68 @@ document.getElementById('btnHelp').addEventListener('touchend', () => {
 })
 
 // boton 3x3
-document.getElementById('level_size_3').addEventListener('touchstart', () => {
-    reset()
-    puzzle_size = 3
-    cant_bloques = puzzle_size * puzzle_size
-    start('show') // show para que solo muestre la imagen, y no se creen los bloques y habiliten los movimientos.
+document.getElementById('level_size_3').addEventListener('click', () => {
+    pregunta(3)
 })
 
 // boton 4x4
-document.getElementById('level_size_4').addEventListener('touchstart', () => {
-    reset()
-    puzzle_size = 4
-    cant_bloques = puzzle_size * puzzle_size
-    start('show') // show para que solo muestre la imagen, y no se creen los bloques y habiliten los movimientos.
+document.getElementById('level_size_4').addEventListener('click', () => {
+    pregunta(4)
 })
 
 // boton 5x5
-document.getElementById('level_size_5').addEventListener('touchstart', () => {
-    reset()
-    puzzle_size = 5
-    cant_bloques = puzzle_size * puzzle_size
-    start('show') // show para que solo muestre la imagen, y no se creen los bloques y habiliten los movimientos.
+document.getElementById('level_size_5').addEventListener('click', () => {
+    pregunta(5)
 })
 
 // boton 6x6
-document.getElementById('level_size_6').addEventListener('touchstart', () => {
-    reset()
-    puzzle_size = 6
-    cant_bloques = puzzle_size * puzzle_size
-    start('show') // show para que solo muestre la imagen, y no se creen los bloques y habiliten los movimientos.
+document.getElementById('level_size_6').addEventListener('click', () => {
+    pregunta(6)
 })
 
+// juego nuevo.
+function new_game(){
+    reset()
+    start()
+    setTimeout(() => {
+        scramble(500)
+    }, 0);
+}
+
+
+
+// seleccion de nivel.
+function level(size){
+    reset()
+    puzzle_size = size
+    cant_bloques = puzzle_size * puzzle_size
+    start('show') // show para que solo muestre la imagen, y no se creen los bloques y habiliten los movimientos.
+}
+
+// SweetAlert2 pregunta.
+function pregunta(size,newGame = false){
+    // num indica si se jugo previamente, ya que se genera en scramble().
+    // si se jugo previamente, realizo la pregunta.
+    if(num){
+        Swal.fire({
+            title: 'Cancelar juego?',
+            text: "",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ok'
+        }).then((result) => {
+            if (result.value) {
+                level(size)
+                num = false // reset de la variable.
+            }
+        })
+    }else{
+        if(newGame){
+            new_game()
+            return
+        }
+        level(size)
+    }
+}
