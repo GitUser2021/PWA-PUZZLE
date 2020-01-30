47c40631c171bfb8c76bf8f6e1b41c1ae1eb5905
GitUser2021 <58796929+GitUser2021@users.noreply.github.com>
2020-01-30 01:37:09 -0300

    agrego funcion para ayuda

--- js/main.js
+++ js/main.js
@@ -9,21 +9,50 @@ let ref_div_main = document.getElementById('main')
 let last_row_index = []
 let first_row_index = []
 
+// calcula la cant. de bloques totales.
+function cant_bloques() {
+    return CANT_BLOQUES = PUZZLE_SIZE * PUZZLE_SIZE
+}
 
+// mostrar imagen como ayuda.
+function show_image(show) {
+    if (show) {
+        ref_div_main.style.backgroundImage = IMAGE
+        ref_div_main.style.backgroundSize = 'cover'
+        objArray.forEach(bloque => {
+            bloque.bloque.style.display = 'none'
+        })
+    } else {
+        ref_div_main.style.backgroundImage = ''
+        objArray.forEach((bloque, key) => {
+            if (key == CANT_BLOQUES - 1) {
+                bloque.bloque.style.display = 'none'
+                return
+            }
+            bloque.bloque.style.display = 'block'
+        }
+        )
+    }
+}
 
-// funcion para boton New Game.
+// boton New Game.
 document.getElementById('btnNewGame').addEventListener('touchstart', () => {
     reset()
-    start() 
+    start()
     setTimeout(() => {
         scramble(500)
-    },0);
+    }, 0);
 })
 
-// calcula la cant. de bloques totales.
-function cant_bloques(){
-     return CANT_BLOQUES = PUZZLE_SIZE * PUZZLE_SIZE
-}
+// boton Help.
+document.getElementById('btnHelp').addEventListener('touchstart', () => {
+    show_image(true)
+})
+document.getElementById('btnHelp').addEventListener('touchend', () => {
+    show_image(false)
+})
+
+
 
 document.getElementById('level_size_3').addEventListener('touchstart', () => {
     reset()
